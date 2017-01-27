cbec48f85a4129af51e1e4d9c9362b3cafe82224
Ez_Dor <Dor348@gmail.com>
2017-01-27 17:21:02 +0200

    mmn16.

--- src/Mmn16/Q1/Tester.java
+++ src/Mmn16/Q1/Tester.java
@@ -3,9 +3,10 @@ package Mmn16.Q1;
 
 public class Tester {
     public static void main(String[] args) {
-        ClientInChatRoom c1 = new ClientInChatRoom("1");
-       Thread one = new Thread(c1);
-        one.start();
+        ServerGUI serverGUI = new ServerGUI(7777);
+        ClientGUI c1 = new ClientGUI("localhost", 7777);
+        ClientGUI c2 = new ClientGUI("localhost", 7777);
+        ClientGUI c3 = new ClientGUI("localhost", 7777);
 
     }
 
